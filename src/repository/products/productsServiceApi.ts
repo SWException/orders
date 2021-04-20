b305767ec96a5164012341d707be8eb0688b5cfd
Ivan Furlan <ivanfurlan98@gmail.com>
2021-04-20 17:53:30 +0200

    Fatto ProductsServiceAPI

--- src/repository/products/productsServiceApi.ts
+++ src/repository/products/productsServiceApi.ts
@@ -1,8 +1,25 @@
-//import fetch from "node-fetch";
+import fetch from "node-fetch";
 import ProductsService from "../productsService";
-// products/stock/{id} 
+
 export default class ProductsServiceAPI implements ProductsService {
     public async updateStock(PRODUCT_ID: string, QUANTITY: number, TOKEN: string): Promise<boolean> {
-        return (PRODUCT_ID && QUANTITY && TOKEN)? true : false;
+        return await fetch(process.env.SERVICES + `/products/stock/${PRODUCT_ID}`, {
+            method: 'PATCH',
+            headers: {
+                Authorization: TOKEN
+            },
+            body: JSON.stringify({
+                quantity: QUANTITY
+            })
+        })
+        .then(async responseProduct => await responseProduct.json())
+        .then(res => {
+            if (res.status == "success")
+                return true;
+            return false;
+        })
+        .catch((err: Error) => {
+            throw new Error("Error updating product "+ PRODUCT_ID +" stock. Details: " + err.message);
+        })
     }
 }

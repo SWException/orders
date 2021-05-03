eeae03bcf3e6df9424d5d745dbc1eb8088609400
Marco <marco.canovese@studenti.unipd.it>
2021-05-03 14:28:09 +0200

    fix2

--- src/repository/database/dynamo.ts
+++ src/repository/database/dynamo.ts
@@ -8,7 +8,7 @@ export default class Dynamo implements Database {
 
     public async createOrder (ORDER_ID: string, USERNAME: string, SHIPPING: string, BILLING: string,
         CART: { [key: string]: any; }, STATUS: string, SHIPPING_FEE: number): Promise<any> {
-        const TOTAL = CART.total + SHIPPING_FEE);
+        const TOTAL = CART.total + SHIPPING_FEE;
         const PARAMS = {
             TableName: Dynamo.TABLE_NAME,
             Item: {
